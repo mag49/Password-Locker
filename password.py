3b7a3ea9431f9bf0aca834d613225f806a5eaf2b
Ngethe <ciru930@gmail.com>
2020-01-26 12:03:15 +0300

    created a class for user credential

---
+++ password.py
@@ -0,0 +1,13 @@
+import pyperclip
+
+class Credential:
+
+  """
+    Class that generates new instances of credentials.
+    """
+
+    credential_list = [] # Empty credential list
+    
+
+    def __init__(self,username,email,password):
+   
