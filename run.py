3b5e40d2f5a721d4876fb7392d8498261ef2f08a
Ngethe <ciru930@gmail.com>
2020-01-27 10:14:41 +0300

    added function that creates new information data of a new user

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ import string
 import time
 import pyperclip
 
-def create_credentials(user_name, password):
+def create_credentials(user_name, email, password):
     '''
     function that creates new credentials
     '''
-    new_credential = Credential(user_name,password)
+    new_credential = Credential(user_name,email, password)
     return new_credential
 
 def save_credentials(credential):
@@ -26,8 +26,27 @@ def find_existing_credentials(user_name):
     '''
     return Credential.credentials_exist(user_name)
 
+def find_existing_credentials(email):
+    '''
+    function to test if credentials exist
+    '''
+    return Credential.credentials_exist(email)
+    
 
-def authenticate_credentials(user_name, password):
+def authenticate_credentials(user_name, email,password):
     '''
     '''
     return Credential.authenticate_credentials(user_name,password)
+
+def user_information(account_name,account_user_name, account_email, account_password):
+    '''
+    Function to authenticate and log in a user
+    '''
+    information = UserInformation(account_name, account_user_name, account_email, account_password)
+    return information 
+
+def create_new_information(myinformation):
+    '''
+    Function that creates new information to save user password
+    '''
+    myinformation.create_password()
